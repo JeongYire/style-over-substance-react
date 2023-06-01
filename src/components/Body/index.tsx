62e13d0a1e48b13acb53af95be8a44a2b3d57b77
JeongYire <wotjd0611@gmail.com>
2023-06-01 18:20:50 +0900

    commit2

--- src/components/Body/index.tsx
+++ src/components/Body/index.tsx
@@ -1,14 +1,9 @@
-<<<<<<< HEAD
-import React from 'react';
+
 import Title from './Title';
-=======
 import React, { useState } from 'react';
 import FormBox from './FormBox';
-<<<<<<< HEAD
-import { BoxType } from '../../types';
->>>>>>> 5267a98488512f0f0b8b6b04371a4ffc7c7108f2
-=======
->>>>>>> parent of 5267a98 (add)
+
+
 
 
 function App() {
@@ -17,15 +12,7 @@ function App() {
 
     return (
         <div style={{backgroundColor:'red'}}>
-<<<<<<< HEAD
             <Title />
-=======
-            {
-                boxArray.map(obj => {
-                    return boxArray;
-                })
-            }
->>>>>>> 5267a98488512f0f0b8b6b04371a4ffc7c7108f2
         </div>
     );
 }
