59c54a8b39a615d684b9cfe5d7d127f83ed2f721
JeongYire <wotjd0611@gmail.com>
2023-06-10 22:54:40 +0900

    commit2

--- src/components/FormContainer/FormBox/index.tsx
+++ src/components/FormContainer/FormBox/index.tsx
@@ -17,7 +17,7 @@ export default (params : { id : number, type : BoxType, index : number }) => {
     const required = useRef<boolean>(false);
     const requiredInput = useRef<any>();
 
-    //주석
+    //주석2
     const CompleteEdit = () => {
         title.current = (titleInput.current as HTMLInputElement).value;
         content.current = (contentInput.current as HTMLInputElement).value;
