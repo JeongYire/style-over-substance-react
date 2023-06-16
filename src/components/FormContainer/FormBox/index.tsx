3ca78b49533c6763e9b8439bb2f93d85cda58692
JeongYire <wotjd0611@gmail.com>
2023-06-16 23:51:05 +0900

    co

--- src/components/FormContainer/FormBox/index.tsx
+++ src/components/FormContainer/FormBox/index.tsx
@@ -31,7 +31,7 @@ export default (params : { id : number, type : BoxType, index : number }) => {
     },[params.type])
     
 
-    //주석5
+    //주석6
     const CompleteEdit = () => {
         title.current = (titleInput.current as HTMLInputElement).value;
         content.current = (contentInput.current as HTMLInputElement).value;
